e5e86296937ea250479d20448a18d95a9859dd0d
justguerra <guerra@foxway.com.br>
2015-07-07 02:59:35 -0300

    Finished Simple Interface.

--- AssemblyScreen.java
+++ AssemblyScreen.java
@@ -6,14 +6,21 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTextPane;
 import javax.swing.JTextArea;
+
 import java.awt.Panel;
 import java.awt.Color;
+
 import javax.swing.JTable;
+
 import java.awt.Canvas;
+
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.ImageIcon;
+
 import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 
 public class AssemblyScreen extends JFrame {
@@ -48,21 +55,24 @@ public class AssemblyScreen extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JTextArea textArea = new JTextArea();
-		textArea.setText("0:\n1:\n2:\n3:\n4:\n5:\n6:\n7:\n8:\n9:\n10:\n11:\n12:\n13:\n14:\n15:\n16:\n17:\n18:\n19:\n20:\n21:\n22:\n23:\n24:\n25:\n26:\n27:\n28:\n29:\n30:\n31:\n32:\n33:\n34:\n35:\n36:\n37:\n38:\n39:\n40:");
-		textArea.setWrapStyleWord(true);
-		textArea.setRows(32);
-		textArea.setBounds(6, 6, 20, 665);
-		contentPane.add(textArea);
+		JTextArea lineNumber = new JTextArea();
+		lineNumber.setText("0:\n1:\n2:\n3:\n4:\n5:\n6:\n7:\n8:\n9:\n10:\n11:\n12:\n13:\n14:\n15:\n16:\n17:\n18:\n19:\n20:\n21:\n22:\n23:\n24:\n25:\n26:\n27:\n28:\n29:\n30:\n31:\n32:\n33:\n34:\n35:\n36:\n37:\n38:\n39:\n40:");
+		lineNumber.setWrapStyleWord(true);
+		lineNumber.setRows(32);
+		lineNumber.setBounds(6, 6, 20, 665);
+		contentPane.add(lineNumber);
 		
-		JTextPane txtpnDddd = new JTextPane();
-		txtpnDddd.setBounds(33, 6, 115, 658);
-		contentPane.add(txtpnDddd);
+		JTextPane textAssembly = new JTextPane();
+		textAssembly.setBounds(33, 6, 115, 658);
+		contentPane.add(textAssembly);
 		
+		String[] columnNames = {"Memória", "Dados"};
 		
+		Object[][] data = {{"0x12341234", ""},{"0x87654321", ""},{"0x76543210", ""}};
+
+	//	Object[][] data = new Object[40][40];
 		
-		
-		table = new JTable();
+		table = new JTable(data, columnNames);
 		table.setBounds(728, 128, 347, 109);
 		contentPane.add(table);
 		
@@ -71,9 +81,7 @@ public class AssemblyScreen extends JFrame {
 		lblNewLabel.setBounds(174, 128, 520, 520);
 		contentPane.add(lblNewLabel);
 		
-		JButton btnExecutar = new JButton("Executar");
-		btnExecutar.setBounds(38, 669, 117, 29);
-		contentPane.add(btnExecutar);
+		
 		
 		JLabel lblPortasAbertas = new JLabel("Portas Abertas:");
 		lblPortasAbertas.setBounds(174, 72, 177, 16);
@@ -83,5 +91,77 @@ public class AssemblyScreen extends JFrame {
 		openBars.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
 		openBars.setBounds(174, 88, 520, 29);
 		contentPane.add(openBars);
+		
+		
+		JButton btnExecutar = new JButton("Executar");
+		JButton btnNextLine = new JButton("Próxima Linha");
+		JButton btnParar = new JButton("Parar");
+		JLabel lblExecutandoLinha = new JLabel("Executando Linha:");
+		JLabel linhaSendoExecutada = new JLabel("0");
+		
+		btnExecutar.setBounds(38, 669, 117, 29);
+		contentPane.add(btnExecutar);
+		
+	
+		btnNextLine.setEnabled(false);
+		btnNextLine.setBounds(292, 669, 117, 29);
+		contentPane.add(btnNextLine);
+		
+		
+		lblExecutandoLinha.setBounds(421, 674, 124, 16);
+		contentPane.add(lblExecutandoLinha);
+		
+		
+		linhaSendoExecutada.setBounds(539, 674, 90, 16);
+		contentPane.add(linhaSendoExecutada);
+		
+		
+		btnParar.setEnabled(false);
+		btnParar.setBounds(174, 669, 117, 29);
+		contentPane.add(btnParar);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(519, 661, -21, -13);
+		contentPane.add(panel);
+		
+		CmdAssembly cmdAssembly = new CmdAssembly();
+		
+		btnExecutar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {		
+				String portasAbertas = cmdAssembly.execute(table, textAssembly, openBars);
+				btnExecutar.setEnabled(false);
+				btnNextLine.setEnabled(true);
+				btnParar.setEnabled(true);
+				textAssembly.setEnabled(false);
+				linhaSendoExecutada.setText("0");
+				
+			}
+		});
+		
+		
+		btnNextLine.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String linhaS = linhaSendoExecutada.getText();
+				int linha = Integer.parseInt(linhaS);
+				linha++;
+				linhaS = String.valueOf(linha);
+				linhaSendoExecutada.setText(linhaS);
+				cmdAssembly.executeLine(linha);
+			}
+		});
+		
+	
+		btnParar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btnExecutar.setEnabled(true);
+				btnNextLine.setEnabled(false);
+				btnParar.setEnabled(false);
+				textAssembly.setEnabled(true);
+			}
+		});
+		//Todo: Quanto não tem mais linhas?
+		
+		
+		
 	}
 }
--- CmdAssembly.java
+++ CmdAssembly.java
@@ -1,10 +1,61 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextPane;
+import javax.swing.JTextArea;
+import java.awt.Panel;
+import java.awt.Color;
+import javax.swing.JTable;
+import java.awt.Canvas;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.util.LinkedList;
+
 
 public class CmdAssembly {
 	
-	public static void main(String[] args){
-
-		//[Entrada]: não sei como sera o input
-		String cmd = "ADD 1F, 20";
+	private String [] comandos;
+	
+	public int getRowByValue(JTable model, String value) {
+		//http://stackoverflow.com/questions/14196974/getting-a-row-index-in-a-jtable-using-only-a-value-included-in-the-row
+	    for (int i = model.getRowCount() - 1; i >= 0; --i) {
+	        for (int j = model.getColumnCount() - 1; j >= 0; --j) {
+	            if (model.getValueAt(i, j).equals(value)) {
+	                return i;
+	            }
+	        }
+	    }
+	    return -1;
+	}	
+ 
+	
+	public void updateMemoria(JTable table, String valor, String endereco){
+		//Procura qual é a linha que está o endereço -> getRowByValue(table, endereco)
+		//Atualizar a coluna 1 = valor
+		table.setValueAt(valor, getRowByValue(table, endereco), 1);
+	}
+	
+	public void updateBus(JLabel openBars, Integer[] portas){
+		String abertas = "";
+		for(int i=0;i<=portas.length-1;i++){
+			abertas += portas[i].toString();
+			if (i<portas.length-1) abertas += ", ";
+		}
+		abertas += ".";
+		
+		openBars.setText(abertas);
+		
+	}
+	
+	public void executeLine(Integer i){
+		
+		//Verifica qual o comando e roda o código do comando.
 		
 		//Declaracao das strings
 		String command ="";
@@ -17,7 +68,7 @@ public class CmdAssembly {
 
 		//Separacao  do cmd em cmmand, ax e bx
 		int counter = 0;
-		for(char c : cmd.toCharArray()){
+		for(char c : comandos[i].toCharArray()){
 			if (c == ' ') counter++;
 			if (counter == 0){
 				command += c;
@@ -36,10 +87,6 @@ public class CmdAssembly {
 		System.out.println("[DEBUG] BX:" + bx);
 		
 		if(command.equalsIgnoreCase("") || ax.equalsIgnoreCase("") || bx.equalsIgnoreCase("")) throw new IllegalArgumentException("Parametros nao podem ser nulos");
-		
-		//Transformando as Strings em inteiros
-		//int AX = Integer.parseInt(ax); 
-		//int BX = Integer.parseInt(bx);
 		
 		//Transformando em decimal
 		Integer AXDec = toDec(ax);
@@ -109,6 +156,32 @@ public class CmdAssembly {
 			default:
 				throw new IllegalArgumentException("Comando Invalido: " + command + " [>] tente CMD ax, bx");
 		}
+		
+		//Retorna as portas que ficarão abertas e As memórias que mudarão
+	
+	}
+	
+	public String execute(JTable table, JTextPane txtAssembly, JLabel openBars){
+		
+		
+		//Limpar comandos
+		
+		//Cria a lista ligada ou matriz com o código.
+		String codigo = txtAssembly.getText();
+		comandos = codigo.split("\\r?\\n");
+		
+		//updateBus(openBars, abre );
+		
+		
+		//Rodar comando da primeira linha.
+		executeLine(0);
+			//Verifica qual o comando e roda o código do comando.
+			//Retorna as portas que ficarão abertas e 
+			//Muda as memórias que mudarão
+	
+		
+		return null;
+		
 	
 	}
 	
